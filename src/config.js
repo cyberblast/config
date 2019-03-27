4200a59bdd37e2ea79944210736d0df14d0d6382
cyberblast <git@cyberblast.org>
2019-03-27 22:12:35 +0100

    async

--- src/config.js
+++ src/config.js
@@ -3,38 +3,24 @@ module.exports = mod;
 
 const fs = require("fs");
 
-function loadFile(error, complete, path){
-  fs.readFile(path, function (fsError, data) {
-    if (fsError) {
-      error(fsError);
-      return;
-    }
-    
-    let file;
-    try{
-      file = JSON.parse(data.toString());
-    } catch(e){
-      error(e);
-      return;
-    }
-    complete(file);
+async function loadFileAsync(filePath){
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, function (fsError, data) {
+      if (fsError) {
+        reject(fsError);
+        return;
+      }
+      resolve(JSON.parse(data.toString()));
+    });
   });
 };
 
-mod.load = function(onError, onSuccess, filePath, forceReload = false){
+mod.load = async function(filePath, forceReload = false){
   if(forceReload && mod.settings !== undefined){
     delete mod.settings;
   }
   if(mod.settings === undefined){
-    loadFile(
-      onError, 
-      file => {
-        mod.settings = file;
-        onSuccess(file);
-      },
-      filePath
-    );
-  } else {
-    onSuccess(mod.settings);
+    mod.settings = await loadFileAsync(filePath);
   }
-};
+  return mod.settings;
+}
