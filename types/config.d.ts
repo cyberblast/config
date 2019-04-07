43dbef840ed7877eda1d2ce43b15d22b31e23d01
cyberblast <git@cyberblast.org>
2019-04-07 18:45:10 +0200

    .d.ts

---
+++ types/config.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for @cyberblast/config v0.0.7
+// Project: https://github.com/cyberblast/config
+// Definitions by: cyberblast <https://github.com/cyberblast>
+
+declare class Config {
+  constructor(filePath: string);
+  load(): Promise<{ [key: string]: any }>;
+}
+
+export = Config;
