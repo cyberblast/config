5246348a0f732ac68c4ce89628e932099e5e5f98
cyberblast <git@cyberblast.org>
2019-04-07 21:09:45 +0200

    type

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
 
+/**
+ * Load file
+ * @param {string} filePath - Path of json file to load
+ * @return { Promise<{ [key: string]: any }> } Promise containing file content, parsed to JSON object
+ */
 async function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, function(fsError, data) {
@@ -12,7 +17,22 @@ async function readFileAsync(filePath) {
   });
 }
 
+/**
+ * Class Config for loading a single json config file
+ * @constructor
+ * @param {string} filePath - Path of json file to load
+ */
 function Config(filePath) {
+
+  /**
+   * @type { Object.<string, any> }
+   */
+  this.settings = undefined;
+
+  /**
+   * Load Config file
+   * @return { Promise<{ [key: string]: any }> } Promise containing loaded settings
+   */
   this.load = async function() {
     if (this.settings !== undefined) {
       delete this.settings;
