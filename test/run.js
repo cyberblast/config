beb0fe3e3d0014703ae2e178151feafeae55d0da
cyberblast <git@cyberblast.org>
2019-12-15 22:19:23 +0100

    improve test

--- test/run.js
+++ test/run.js
@@ -1,12 +1,22 @@
 const Config = require('../src/config.js');
 const configFile = './test/config.json';
+let settings;
 
 async function run() {
   console.info('running test for Config module');
   const config = new Config(configFile);
-  const settings = await config.load();
+  settings = await config.load();
   console.info('file loaded');
-  if (settings.some !== 'thing') throw 'Settings content not loaded!';
+}
+
+function validate() {
+  if (settings == null || settings.some !== 'thing') throw 'Settings content not ok!';
   console.info('content ok');
 }
-run();
+
+run()
+  .then(validate)
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
